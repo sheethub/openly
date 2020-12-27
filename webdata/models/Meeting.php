f05b15834496fe2c1b8726026800f4049b98f48b
Ronny Wang <ronnywang@gmail.com>
2020-12-27 14:53:11 +0800

    add Meeting->getName()

--- webdata/models/Meeting.php
+++ webdata/models/Meeting.php
@@ -1,11 +1,29 @@
 <?php
 
+class MeetingRow extends Pix_Table_Row
+{
+    public function getName()
+    {
+        if ($this->session_type == 1) { // 全院委員會
+            return sprintf("第%02d屆第%02d會期第%02d次會議", $this->term, $this->session_period, $this->session_times);
+        } else if ($this->session_type == 2) { // 常會
+            return sprintf("第%02d屆第%02d會期第%02d次會議", $this->term, $this->session_period, $this->session_times);
+        } else if ($this->session_type == 3) { // 臨時會
+            return sprintf("第%02d屆第%02d會期第%02d次臨時會第%02d次會議", $this->term, $this->session_period, $this->session_times, $this->meeting_times);
+        } else if ($this->session_type == 4) { // 臨時會全院委員會
+            return sprintf("第%02d屆第%02d會期第%02d次臨時會第%02d次全院委員會", $this->term, $this->session_period, $this->session_times, $this->meeting_times);
+        }
+
+    }
+}
+
 class Meeting extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'meeting';
         $this->_primary = 'meeting_id';
+        $this->_rowClass = 'MeetingRow';
 
         $this->_columns['meeting_id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['term'] = array('type' => 'int');
