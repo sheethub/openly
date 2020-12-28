fea247c6f551604cf2ac75af876ac40adfa85103
Ronny Wang <ronnywang@gmail.com>
2020-12-28 20:01:22 +0800

    add S3Lib to put file to s3

---
+++ webdata/models/S3Lib.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class S3Lib
+{
+    protected static $_s3 = null;
+    public static function getS3()
+    {
+        if (is_null(self::$_s3)) {
+            include(__DIR__ . '/../stdlibs/aws/aws-autoloader.php');
+            self::$_s3 = new Aws\S3\S3Client([
+                'region' => 'ap-northeast-1',
+                'version' => 'latest',
+            ]);
+        }
+        return self::$_s3;
+    }
+
+    public function put($local_file, $target_path)
+    {
+        $s3 = self::getS3();
+        $ret = $s3->putObject([
+            'Bucket' => 'twlydata',
+            'Key' => $target_path,
+            'Body' => file_get_contents($local_file),
+            'ACL' => 'public-read',
+            'ContentType' => mime_content_type($local_file),
+            'CacheControl' => 'max-age=31536000,public'
+        ]);
+    }
+}
