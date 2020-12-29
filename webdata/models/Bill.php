b67645c7489f01013a16b5681feef50a232e8194
Ronny Wang <ronnywang@gmail.com>
2020-12-30 00:17:54 +0800

    add BillRow

--- webdata/models/Bill.php
+++ webdata/models/Bill.php
@@ -1,11 +1,16 @@
 <?php
 
+class BillRow extends Pix_Table_Row
+{
+}
+
 class Bill extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'bill';
         $this->_primary = 'billno';
+        $this->_rowClass = 'BillRow';
 
         $this->_columns['billno'] = array('type' => 'char', 'size' => 16);
         $this->_columns['wordno'] = array('type' => 'varchar', 'size' => 64);
