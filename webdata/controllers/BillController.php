a84450b46edf26e108d6a3c9ba3fcaa847140ba3
Ronny Wang <ronnywang@gmail.com>
2020-12-30 16:46:26 +0800

    關係文書內容顯示功能

--- webdata/controllers/BillController.php
+++ webdata/controllers/BillController.php
@@ -10,4 +10,34 @@ class BillController extends Pix_Controller
         }
         $this->view->bill = $bill;
     }
+
+    public function docAction()
+    {
+        list(, /*bill*/, /*doc*/, $billno) = explode('/', $this->getURI());
+        if (!$bill = Bill::find(strval($billno))) {
+            return $this->notfound();
+        }
+        $this->view->bill = $bill;
+        $this->view->objs = array();
+        if (json_decode($bill->data)->doc_bak_url) {
+            foreach (json_decode($bill->data)->doc_bak_url as $file) {
+                $this->view->objs = array_merge($this->view->objs, array(json_decode(gzdecode('' . S3Lib::get("data/bill-json/{$file}.gz")))));
+            }
+        }
+
+        if ($_GET['img']) {
+            foreach ($this->view->objs as $idx => $obj) {
+                foreach ($obj->attachments as $attachment) {
+                    if ($idx == $_GET['idx'] and $attachment->file_name == $_GET['img']) {
+                        $ext = pathinfo($attachment->file_name, PATHINFO_EXTENSION);
+                        header('Content-Type: image/' . $ext);
+                        echo base64_decode($attachment->content);
+                        exit;
+                    }
+                }
+                echo "not found";
+                exit;
+            }
+        }
+    }
 }
