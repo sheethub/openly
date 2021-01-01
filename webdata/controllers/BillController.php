48b1110eabe98b25addb1da7b4b244b7e810a819
Ronny Wang <ronnywang@gmail.com>
2021-01-01 23:40:05 +0800

    加上議案流程

--- webdata/controllers/BillController.php
+++ webdata/controllers/BillController.php
@@ -20,6 +20,15 @@ class BillController extends Pix_Controller
         $this->view->bill = $bill;
     }
 
+    public function historyAction()
+    {
+        list(, /*bill*/, /*history*/, $id) = explode('/', $this->getURI());
+        if (!$bill = Bill::find(strval($id))) {
+            return $this->notfound();
+        }
+        $this->view->bill = $bill;
+    }
+
     public function docAction()
     {
         list(, /*bill*/, /*doc*/, $billno) = explode('/', $this->getURI());
