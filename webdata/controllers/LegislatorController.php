bfb6540c64f94e8702f42696650ac832104bb620
Ronny Wang <ronnywang@gmail.com>
2021-01-04 20:55:54 +0800

    增加顯示各屆立委的功能

--- webdata/controllers/LegislatorController.php
+++ webdata/controllers/LegislatorController.php
@@ -4,5 +4,15 @@ class LegislatorController extends Pix_Controller
 {
     public function indexAction()
     {
+        $term = Legislator::search(1)->max('term')->term;
+        return $this->redirect('/legislator/term/' . $term);
+    }
+
+    public function termAction()
+    {
+        list(, /*legislator*/, /*term*/, $term) = explode('/', $this->getURI());
+        $term = intval($term);
+        $this->view->term = $term;
+        $this->view->max_term = Legislator::search(1)->max('term')->term;
     }
 }
